b387c2da1035ce092d7e8fc35b203482a06feeb6
Chris Richmond <pal_user@noEmail.com>
2018-10-02 17:08:26 -0600

    WIP2

--- src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
@@ -44,7 +44,7 @@ public class JdbcTimeEntryRepository implements TimeEntryRepository {
     }
 
     @Override
-    public TimeEntry find(Long id) {
+    public TimeEntry find(long id) {
         return jdbcTemplate.query(
                 "SELECT id, project_id, user_id, date, hours FROM time_entries WHERE id = ?",
                 new Object[]{id},
@@ -57,7 +57,7 @@ public class JdbcTimeEntryRepository implements TimeEntryRepository {
     }
 
     @Override
-    public TimeEntry update(Long id, TimeEntry timeEntry) {
+    public TimeEntry update(long id, TimeEntry timeEntry) {
         jdbcTemplate.update("UPDATE time_entries " +
                         "SET project_id = ?, user_id = ?, date = ?,  hours = ? " +
                         "WHERE id = ?",
@@ -71,7 +71,7 @@ public class JdbcTimeEntryRepository implements TimeEntryRepository {
     }
 
     @Override
-    public void delete(Long id) {
+    public void delete(long id) {
         jdbcTemplate.update("DELETE FROM time_entries WHERE id = ?", id);
     }
 
